b0fe6aacde5fbd7a125d42985691df0a7a638af7
temptation4 <30442527+temptation4@users.noreply.github.com>
2026-06-09 18:07:36 +0530

    add next greater value problem

--- ProblemSolving.java
+++ ProblemSolving.java
@@ -660,4 +660,46 @@ return longestCommonSubstring(s,rev);
                 return stack.isEmpty();
 
             }
+
+      /*     Next Greater Element I
+    https://leetcode.com/problems/next-greater-element-i/description/?utm_source=chatgpt.com
+    Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+    Output: [-1,3,-1]
+    Explanation: The next greater element for each value of nums1 is as follows:
+            - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+            - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+            - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1. */
+
+
+        static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+            Stack<Integer> stack = new Stack<>();
+            Map<Integer, Integer> map = new HashMap<>();
+
+            // Build next greater element map
+            for (int num : nums2) {
+
+                while (!stack.isEmpty() && stack.peek() < num) {
+
+                    map.put(stack.pop(), num);
+                }
+
+                stack.push(num);
+            }
+
+            // Remaining elements have no greater element
+            while (!stack.isEmpty()) {
+
+                map.put(stack.pop(), -1);
+            }
+
+            int[] result = new int[nums1.length];
+
+            for (int i = 0; i < nums1.length; i++) {
+
+                result[i] = map.get(nums1[i]);
+            }
+
+            return result;
+        }
 }
